cb497c5f2fd79aa133c5b40a79036981a121be43
Crgio <randaledomman1@gmail.com>
2021-07-19 19:25:40 -0300

    specified some routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { createServer } from 'http';
 import { readFile } from 'fs';
 import { join } from 'path';
+import RouteTo from './routes';
 
 const PORT = process.env.PORT || 5000;
 
 const server = createServer((req, res) => {
-	if (req.url === '/' && req.method === 'GET') {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write('<h1>working</h1>');
+	if (RouteTo.GET_ALL(req)) {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.write({ message: 'working' });
 		return res.end();
+	} else if (RouteTo.GET_ONE(req)) {
+	} else if (RouteTo.POST(req)) {
+	} else if (RouteTo.PUT(req)) {
+	} else if (RouteTo.DELETE(req)) {
 	} else {
 		readFile(join('src', 'views', 'PAGE404.html'), (err, page) => {
 			res.writeHead(404, { 'Content-Type': 'text/html' });

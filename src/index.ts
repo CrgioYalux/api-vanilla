e2864008db6cc909b36402c2501f2e40550e3213
Crgio <randaledomman1@gmail.com>
2021-07-19 17:54:21 -0300

    added a 404 page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import { createServer } from 'http';
+import { readFile } from 'fs';
+import { join } from 'path';
 
 const PORT = process.env.PORT || 5000;
 
-const server = createServer((req, res) => {});
+const server = createServer((req, res) => {
+	if (req.url === '/' && req.method === 'GET') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write('<h1>working</h1>');
+		return res.end();
+	} else {
+		readFile(join('src', 'views', 'PAGE404.html'), (err, page) => {
+			res.writeHead(404, { 'Content-Type': 'text/html' });
+			res.write(page);
+			return res.end();
+		});
+	}
+});
 
 server.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
